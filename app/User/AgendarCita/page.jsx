a9efeeafd4b3e0ed63227d1c4565b7a1e9a714f9
DeyvitSan <223351@ids.upchiapas.edu.mx>
2023-12-06 18:17:53 -0600

    Vista AgendarCita primera version

--- app/User/AgendarCita/page.jsx
+++ app/User/AgendarCita/page.jsx
@@ -1,7 +1,18 @@
-const AgendarCita = () => {
+"use client"
+import Footer from "@/components/Landing/Footer";
+import AgendarCita from "@/components/User/AgendarCita";
+import Header from "@/components/User/Header";
+import NavBar from "@/components/User/NavBar";
+
+const UsuarioAgendarCita = () => {
     return(
         <div>
-        
+            <Header/>
+            <NavBar/>
+            <AgendarCita/>
+            <Footer/>
         </div>
     )
 }
+
+export default UsuarioAgendarCita;
--- components/User/AgendarCita.jsx
+++ components/User/AgendarCita.jsx
@@ -1,6 +1,25 @@
 const AgendarCita = () => {
     return(
-        <></>
+        <div className="flex flex-col gap-5">
+            <h2 className="font-normal text-6xl">Agendar cita</h2>
+            <div className="flex flex-col gap-3">
+                <input className="pl-2 border-[1px] border-[#41414140] rounded-sm w-[450px] h-12" type="text" placeholder="Nombre(s)" />
+                <div className="flex gap-5">
+                    <input className="pl-2 border-[1px] border-[#41414140] rounded-sm w-[215px] h-12" type="text" placeholder="Apellido paterno" />
+                    <input className="pl-2 border-[1px] border-[#41414140] rounded-sm w-[215px] h-12" type="text" placeholder="Apellido materno" />
+                </div>
+                <div className="flex gap-5">
+                    <input className="pl-2 border-[1px] border-[#41414140] rounded-sm w-[215px] h-12" type="email" placeholder="Correo eletrónico" />
+                    <input className="pl-2 border-[1px] border-[#41414140] rounded-sm w-[215px] h-12" type="text" placeholder="Teléfono" />
+                </div>
+                <div className="flex gap-5">
+                    <input type="date" />
+                    <select>
+                        <option value=""></option>
+                    </select>
+                </div>
+            </div>
+        </div>
     )
 }
 
