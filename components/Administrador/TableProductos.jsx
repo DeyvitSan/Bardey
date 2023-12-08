765fa502bb9f247fe234252c4bd3d7930281e0f9
DeyvitSan <223351@ids.upchiapas.edu.mx>
2023-12-08 01:20:23 -0600

    Tabla productos agregada

--- components/Administrador/TableProductos.jsx
+++ components/Administrador/TableProductos.jsx
@@ -1,4 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const TableProductos = () => {
+
+    const [producto, setProducto] = useState([])
+
+    useEffect(() => {
+        const apiUrl = "http://localhost:3001/api/producto";
+        axios.get(apiUrl)
+            .then(({ data }) => {
+                setProducto(data.data)
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    },);
+
     return(
         <div className="bg-[#FFF] w-[130vh] h-[76vh] rounded-lg shadow-2xl">
             <table className="w-full h-full flex flex-col items-center">
@@ -7,20 +24,28 @@ const TableProductos = () => {
                         <th className="font-semibold text-center text-base">ID</th>
                         <th className="font-semibold text-center text-base">Nombre</th>
                         <th className="font-semibold text-center text-base">Precio</th>
-                        <th className="font-semibold text-center text-base">Acciones</th>
+                        <th className="font-semibold text-center text-base ml-28">Acciones</th>
                     </tr>
                 </thead>
                 <tbody className="w-full flex-col">
-                    <tr className="flex ml-10 pt-4 gap-[20px] mr-5">
-                        <td className="font-normal text-center text-xs"></td>
-                        <td className="font-normal text-center text-xs ml-16"></td>
-                        <td className="font-normal text-center text-xs ml-14"></td>
-                        <td className="flex flex-row gap-5">
-                            <button className="bg-green-500 p-2 h-10 text-[#fff] font-medium rounded-md shadow-2xl">Agregar</button>
-                            <button className="bg-sky-400 p-2 h-10 text-[#fff] font-medium rounded-md shadow-2xl">Editar</button>
-                            <button className="bg-red-500 p-2 h-10 text-[#fff] font-medium rounded-md shadow-2xl">Eliminar</button>
-                        </td>
-                    </tr>       
+
+                {producto.map(producto => {
+                    return(
+                        <>
+                            <tr className="flex ml-[75px] pt-4 gap-[20px] mr-5" key={producto.id_producto}>
+                                <td className="font-medium text-center text-sm">{producto.id_producto}</td>
+                                <td className="font-medium text-center text-sm ml-10">{producto.nombre}</td>
+                                <td className="font-medium text-center text-sm ml-14">{producto.precio}</td>
+                                <td className="flex flex-row gap-5 ml-32">
+                                    <button className="bg-green-500 p-2 h-10 text-[#fff] font-medium rounded-md shadow-2xl">Agregar</button>
+                                    <button className="bg-sky-400 p-2 h-10 text-[#fff] font-medium rounded-md shadow-2xl">Editar</button>
+                                    <button className="bg-red-500 p-2 h-10 text-[#fff] font-medium rounded-md shadow-2xl">Eliminar</button>
+                                </td>
+                            </tr>       
+                        </>
+                    )
+                })}
+
                 </tbody>
             </table>
         </div>
